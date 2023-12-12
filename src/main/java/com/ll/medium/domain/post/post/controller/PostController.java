f3092363be4416bfbcb96f7d887a1d57a50d3b09
jae9380 <ljy5314@gmail.com>
2023-12-12 13:11:11 +0900

    Post - write

---
+++ src/main/java/com/ll/medium/domain/post/post/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.ll.medium.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post")
+public class PostController {
+
+}
